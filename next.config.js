928fb5ace817009b8b29b270bf26e072c8182dd1
Adamya kumar <adamya.kumar@heromotocorp.com>
2025-02-16 14:14:52 +0530

    first commit

---
+++ next.config.js
@@ -0,0 +1,21 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.(woff|woff2|eot|ttf|otf)$/i,
+      issuer: { and: [/\.(js|ts|md)x?$/] },
+      type: 'asset/resource',
+    });
+    return config;
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://gyani-transcribe-bucket.s3.amazonaws.com/:path*',
+      },
+    ];
+  },
+}
+
+module.exports = nextConfig
